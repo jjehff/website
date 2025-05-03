9b2eee0d788826bdc9c6b737c20fc14f09d5ecd9
jjehff <152602266+jjehff@users.noreply.github.com>
2025-05-03 01:21:48 -0700

    Update API.jsx

--- my-portfolio/src/pages/API.jsx
+++ my-portfolio/src/pages/API.jsx
@@ -4,7 +4,7 @@ function PhotoAPI() {
   const [imgUrl, setImgUrl] = useState(null);
 
   const fetchPhoto = async () => {
-    const res = await fetch('https://website-2-tab4.onrender.com/');
+    const res = await fetch('https://website-2-tab4.onrender.com/api/photo');
     const data = await res.json();
     setImgUrl(data.url);
   };
